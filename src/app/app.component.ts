aea86d6a1a2ea15d8039d3cb9721b1d74af0e69b
Alex Nordin <apnordin@gmail.com>
2022-01-17 14:49:59 -0500

    Initial set and change state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// This is where we store the state of our app
 export class AppComponent {
   title = 'teammapp';
+  newMemberName = "";
+  members: string[] = [];
+  addMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
